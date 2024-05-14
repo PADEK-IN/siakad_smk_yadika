5483b49a5ad4ff83b198749bfe861f7fc7bc3443
MRoyhanF <froyhan0@gmail.com>
2024-05-14 22:06:44 +0700

    slicing user managemeny page

--- server/routes/web/user/user.controller.js
+++ server/routes/web/user/user.controller.js
@@ -1,3 +1,4 @@
+// user
 export const getUserPage = (req, res) => {
   res.render('pages/admin/user/index.ejs');
 };
@@ -5,16 +6,33 @@ export const addUserPage = (req, res) => {
   res.render('pages/admin/user/add.ejs');
 };
 
+export const editUserPage = (req, res) => {
+  res.render('pages/admin/user/edit.ejs');
+};
+
+
+// student
 export const getStudentPage = (req, res) => {
   res.render('pages/admin/user/students.ejs');
 };
-export const addStudentPage = (req, res) => {
-  res.render('pages/admin/user/student-add.ejs');
+
+export const detailStudentPage = (req, res) => {
+  res.render('pages/admin/user/student-detail.ejs');
 };
 
+export const editStudentPage = (req, res) => {
+  res.render('pages/admin/user/student-edit.ejs');
+};
+
+// teacher
 export const getTeacherPage = (req, res) => {
   res.render('pages/admin/user/teachers.ejs');
 };
-export const addTeacherPage = (req, res) => {
-  res.render('pages/admin/user/teacher-add.ejs');
+
+export const detailTeacherPage = (req, res) => {
+  res.render('pages/admin/user/teacher-detail.ejs');
+};
+
+export const editTeacherPage = (req, res) => {
+  res.render('pages/admin/user/teacher-edit.ejs');
 };
--- server/routes/web/user/user.router.js
+++ server/routes/web/user/user.router.js
@@ -7,10 +7,14 @@ const router = express.Router();
 // Routes
 router.route("/").get(index.getUserPage);
 router.route("/add").get(index.addUserPage);
-router.route("/students").get(index.getStudentPage);
-router.route("/teachers").get(index.getTeacherPage);
-router.route("/student/add").get(index.addStudentPage);
-router.route("/teacher/add").get(index.addTeacherPage);
+router.route("/:id/edit").get(index.editUserPage);
 
+router.route("/students").get(index.getStudentPage);
+router.route("/student/:id").get(index.detailStudentPage);
+router.route("/student/:id/edit").get(index.editStudentPage);
+
+router.route("/teachers").get(index.getTeacherPage);
+router.route("/teacher/:id").get(index.detailTeacherPage);
+router.route("/teacher/:id/edit").get(index.editTeacherPage);
 
 export default router;
