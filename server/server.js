26f0cd9d5dd9b4886102da4161994d2609234d8d
MRoyhanF <froyhan0@gmail.com>
2024-05-15 22:08:55 +0700

    refactor routing admin page

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import express from "express";
-import morgan from "morgan";
+import express from 'express';
+import morgan from 'morgan';
 import session from 'express-session';
 
 // Import middleware
-import {notFoundHandler} from "./middlewares/errors.js";
+import { notFoundHandler } from './middlewares/errors.js';
 
 // Import WEB Pages Router
-import userRouter from "./routes/web/user/user.router.js";
-import schaduleRouter from "./routes/web/schadule/schadule.router.js";
-import classRouter from "./routes/web/class/class.router.js";
-import paymentRouter from "./routes/web/payment/payment.router.js";
-import indexRouter from "./routes/web/index/index.router.js";
+import userRouter from './routes/web/user/user.router.js';
+import schaduleRouter from './routes/web/schadule/schadule.router.js';
+import classRouter from './routes/web/class/class.router.js';
+import paymentRouter from './routes/web/payment/payment.router.js';
+import indexRouter from './routes/web/index/index.router.js';
 
 // Import API Router
 // Admin
-import authRouter from "./routes/api/auth/auth.router.js";
-import adminUserRouter from "./routes/api/admin/users/users.router.js";
-import adminJurusanRouter from "./routes/api/admin/jurusan/jurusan.router.js";
-import adminMataPelajaranRouter from "./routes/api/admin/mata_pelajaran/mata_pelajaran.router.js";
-import adminGuruRouter from "./routes/api/admin/guru/guru.router.js";
-import adminKelasRouter from "./routes/api/admin/kelas/kelas.router.js";
-import adminMuridRouter from "./routes/api/admin/murid/murid.router.js";
+import authRouter from './routes/api/auth/auth.router.js';
+import adminUserRouter from './routes/api/admin/users/users.router.js';
+import adminJurusanRouter from './routes/api/admin/jurusan/jurusan.router.js';
+import adminMataPelajaranRouter from './routes/api/admin/mata_pelajaran/mata_pelajaran.router.js';
+import adminGuruRouter from './routes/api/admin/guru/guru.router.js';
+import adminKelasRouter from './routes/api/admin/kelas/kelas.router.js';
+import adminMuridRouter from './routes/api/admin/murid/murid.router.js';
 // Murid
-import muridRouter from "./routes/api/murid/profile/profile.router.js";
+import muridRouter from './routes/api/murid/profile/profile.router.js';
 // Guru
-import guruRouter from "./routes/api/guru/profile/profile.router.js";
+import guruRouter from './routes/api/guru/profile/profile.router.js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -34,50 +34,50 @@ const __dirname = path.dirname(__filename);
 const staticFolder = path.resolve(__dirname, '../public');
 const viewsFolder = path.resolve(__dirname, '../views');
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(staticFolder));
 app.use(
-    session({
-        // store: new firestoreStore({
-        //     database: db
-        // }),
-        secret: process.env.SESSION_KEY,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            maxAge: 600000 * 24 //set to 24 hours
-        }
-    })
+  session({
+    // store: new firestoreStore({
+    //     database: db
+    // }),
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 600000 * 24, //set to 24 hours
+    },
+  }),
 );
 app.set('view engine', 'ejs');
 app.set('views', viewsFolder);
 
 // Routes WEB Pages
 // Admin
-app.use("/user", userRouter);
-app.use("/schadule", schaduleRouter);
-app.use("/class", classRouter);
-app.use("/payment", paymentRouter);
+app.use('/admin/user', userRouter);
+app.use('/admin/schadule', schaduleRouter);
+app.use('/admin/class', classRouter);
+app.use('/admin/payment', paymentRouter);
 
 // Index
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // ROUTES API
 // Auth
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter);
 // Admin
-app.use("/api/admin/user", adminUserRouter);
-app.use("/api/admin/jurusan", adminJurusanRouter);
-app.use("/api/admin/mata-pelajaran", adminMataPelajaranRouter);
-app.use("/api/admin/guru", adminGuruRouter);
-app.use("/api/admin/kelas", adminKelasRouter);
-app.use("/api/admin/murid", adminMuridRouter);
+app.use('/api/admin/user', adminUserRouter);
+app.use('/api/admin/jurusan', adminJurusanRouter);
+app.use('/api/admin/mata-pelajaran', adminMataPelajaranRouter);
+app.use('/api/admin/guru', adminGuruRouter);
+app.use('/api/admin/kelas', adminKelasRouter);
+app.use('/api/admin/murid', adminMuridRouter);
 // Murid
-app.use("/api/murid", muridRouter);
+app.use('/api/murid', muridRouter);
 // Guru
-app.use("/api/guru", guruRouter);
+app.use('/api/guru', guruRouter);
 
 // Routes Not Found
 app.use(notFoundHandler);
