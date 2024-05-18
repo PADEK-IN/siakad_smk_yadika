5c47bb3bb4397331ea69af2960e674e83e2cd793
fajrCode <fajri42vandi@gmail.com>
2024-05-18 17:00:12 +0700

    fix bug: fix middleware auth

--- server/routes/web/murid/index/index.router.js
+++ server/routes/web/murid/index/index.router.js
@@ -1,13 +1,13 @@
 import express from "express";
 // import {limit} from "../../../middlewares/rateLimiter.js";
 import * as index from "./index.controller.js";
-import {isAuth, isUser } from "../../../../middlewares/isAuthenticated.js";
+import {isAuth, isMurid } from "../../../../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 // Routes
-router.route("/dashboard").get(isAuth, isUser, index.getIndexPage);
-router.route("/profile").get(isAuth, isUser, index.getProfilePage);
+router.route("/dashboard").get(isAuth, isMurid, index.getIndexPage);
+router.route("/profile").get(isAuth, isMurid, index.getProfilePage);
 
 
 export default router;
