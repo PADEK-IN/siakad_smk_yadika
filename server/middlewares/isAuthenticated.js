045073263c181c56eaf237b0e57483fd13855fef
fajrCode <fajri42vandi@gmail.com>
2024-05-18 16:57:53 +0700

    fix bug: middleware auth

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,4 +1,5 @@
 export const isAuth = (req,res,next)=>{
+  console.log(req.session.user)
   if(!req.session.user){
     console.log("Unauthorized user");
     res.redirect("/login");
@@ -17,9 +18,26 @@ export const isAuthLogin = (req,res,next)=>{
   next();
 }
 
-export const isUser = (req,res,next)=>{
-  if(req.session.user.role == "admin") return res.redirect("/admin/dashboard");
-  if(req.session.user.role == "guru") return res.redirect("/teacher/dashboard");
-  if(req.session.user.role == "murid") return res.redirect("/murid/dashboard");
+export const isAdmin = (req,res,next)=>{
+  if(req.session.user.role != "admin"){
+    if(req.session.user.role == "guru") return res.redirect("/teacher/dashboard");
+    if(req.session.user.role == "murid") return res.redirect("/murid/dashboard");
+  }
+  next();
+}
+
+export const isMurid = (req,res,next)=>{
+  if(req.session.user.role != "murid"){
+    if(req.session.user.role == "guru") return res.redirect("/teacher/dashboard");
+    if(req.session.user.role == "admin") return res.redirect("/admin/dashboard");
+  }
+  next();
+}
+
+export const isGuru = (req,res,next)=>{
+  if(req.session.user.role != "guru"){
+    if(req.session.user.role == "admin") return res.redirect("/admin/dashboard");
+    if(req.session.user.role == "murid") return res.redirect("/murid/dashboard");
+  }
   next();
 }
