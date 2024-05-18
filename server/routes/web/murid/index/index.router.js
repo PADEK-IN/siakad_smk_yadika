c506be2e80dfe83e268b2c27b279304ef35eff81
fajrCode <fajri42vandi@gmail.com>
2024-05-18 13:42:11 +0700

    auth: set auth to index murid

--- server/routes/web/murid/index/index.router.js
+++ server/routes/web/murid/index/index.router.js
@@ -1,12 +1,13 @@
 import express from "express";
 // import {limit} from "../../../middlewares/rateLimiter.js";
 import * as index from "./index.controller.js";
+import {isAuth, isUser } from "../../../../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 // Routes
-router.route("/dashboard").get(index.getIndexPage);
-router.route("/profile").get(index.getProfilePage);
+router.route("/dashboard").get(isAuth, isUser, index.getIndexPage);
+router.route("/profile").get(isAuth, isUser, index.getProfilePage);
 
 
 export default router;
