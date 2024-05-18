a0a57c6ece7a16f466597402d60721d3c4649574
fajrCode <fajri42vandi@gmail.com>
2024-05-18 15:59:07 +0700

    auth: set auth to auth web

--- server/routes/web/auth/auth.router.js
+++ server/routes/web/auth/auth.router.js
@@ -1,13 +1,14 @@
 import express from "express";
 // import {limit} from "../../../middlewares/rateLimiter.js";
 import * as index from "./auth.controller.js";
+import {isAuth, isLogin } from "../../../../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 // Routes
 router.route("/").get(index.getIndexPage);
-router.route("/login").get(index.loginPage);
-router.route("/register").get(index.registerPage);
+router.route("/login").get(isLogin, index.loginPage);
+router.route("/register").get(isLogin, index.registerPage);
 
 
 export default router;
