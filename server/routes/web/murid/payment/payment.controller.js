b990e42edb2783a205a12768b03f45ea07478535
MRoyhanF <froyhan0@gmail.com>
2024-05-20 11:53:17 +0700

    slicing add spp and render camera

--- server/routes/web/murid/payment/payment.controller.js
+++ server/routes/web/murid/payment/payment.controller.js
@@ -1,3 +1,6 @@
 export const getPaymentPage = (req, res) => {
     res.render("pages/murid/payment/index");
 };
+export const addPaymentPage = (req, res) => {
+    res.render("pages/murid/payment/add");
+};
--- server/routes/web/murid/payment/payment.router.js
+++ server/routes/web/murid/payment/payment.router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // Routes
 router.route("/").get(index.getPaymentPage);
+router.route("/add").get(index.addPaymentPage);
 
 
 export default router;
