1e94b72476f43d4a9d38c3a74300d2cce74da88e
MRoyhanF <froyhan0@gmail.com>
2024-05-21 13:48:10 +0700

    featch data mata-pelajaran all

--- server/routes/web/admin/mapel/mapel.controller.js
+++ server/routes/web/admin/mapel/mapel.controller.js
@@ -1,5 +1,20 @@
-export const getMapelPage = (req, res) => {
-    res.render("pages/admin/mapel/index.ejs");
+import Mata_Pelajaran from "../../../../models/mata_pelajaran.model.js"
+import { checkValidId, hashids } from "../../../../helpers/isValidId.js";
+
+export const getMapelPage = async (req, res) => {
+  try {
+    const dataMapel = await Mata_Pelajaran.findAll();
+    const mapel = dataMapel.map((mapel) => {
+        return {
+          ...mapel.dataValues,
+          id: hashids.encode(mapel.id)
+        };
+      });
+    res.render('pages/admin/mapel/index.ejs', {mapel});
+  } catch(err){
+    console.log(err.message);
+    res.render("pages/errors/500");
+  }
   };
 
 export const addMapelPage = (req, res) => {
