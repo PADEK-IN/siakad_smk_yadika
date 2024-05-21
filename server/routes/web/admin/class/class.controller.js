ef10d2558baedaf60a9142f0a4764cce24f90482
MRoyhanF <froyhan0@gmail.com>
2024-05-21 23:45:33 +0700

    feath all class for admin

--- server/routes/web/admin/class/class.controller.js
+++ server/routes/web/admin/class/class.controller.js
@@ -1,6 +1,21 @@
-export const getClassPage = (req, res) => {
-    res.render("pages/admin/class/index.ejs");
-  };
+import Kelas from '../../../../models/kelas.model.js';
+import { checkValidId, hashids } from '../../../../helpers/isValidId.js';
+
+export const getClassPage = async(req, res) => {
+  try {
+    const dataClass = await Kelas.findAll();
+    const kelas = dataClass.map((kelas) => {
+      return {
+        ...kelas.dataValues,
+        id: hashids.encode(kelas.id),
+      };
+    });
+    res.render("pages/admin/class/index.ejs", {kelas});
+  } catch (err) {
+    console.log(err.message);
+    res.render('pages/errors/500');
+  }};
+
 export const addClassPage = (req, res) => {
     res.render("pages/admin/class/add.ejs");
   };
