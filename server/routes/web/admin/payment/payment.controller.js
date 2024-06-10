273e38b202ec17287f688b0b917d23dbfbaf8c35
fajrCode <fajri42vandi@gmail.com>
2024-06-10 16:16:26 +0700

    fix: admin transaksi

--- server/routes/web/admin/payment/payment.controller.js
+++ server/routes/web/admin/payment/payment.controller.js
@@ -1,5 +1,6 @@
 import Spp from '../../../../models/spp.model.js';
-import Guru from "../../../../models/guru.model.js";
+import Murid from '../../../../models/murid.model.js';
+import Transaksi from '../../../../models/transaksi.model.js';
 import { checkValidId, hashids } from '../../../../helpers/isValidId.js';
 
 export const getPaymentPage = async(req, res) => {
@@ -45,11 +46,64 @@ export const editPaymentPage = async(req, res) => {
   };
 
   // Transaction
-export const getTransactionPage = (req, res) => {
-    res.render("pages/admin/payment/transactions.ejs");
-  };
+export const getTransactionPage = async (req, res) => {
+  try {
+    const dataTransaksi = await Transaksi.findAll({
+      include: [
+        {
+            model: Spp,
+        },
+        {
+            model: Murid,
+            attributes: ["nama"]
+        }
+      ]
+    });
 
-export const acceptTransactionPage = (req, res) => {
-    res.render("pages/admin/payment/transaction-request.ejs");
-  };
+    const data = dataTransaksi.map((Transaksi) => {
+        return {
+          ...Transaksi.dataValues,
+          id: hashids.encode(Transaksi.id),
+          id_murid: hashids.encode(Transaksi.id_murid),
+          id_spp: hashids.encode(Transaksi.id_spp)
+        };
+    });
+    
+    res.render("pages/admin/payment/transactions.ejs", {data});
+  } catch (err) {
+      console.log(err.message);
+      res.render("pages/errors/500.ejs");
+  }
+};
+
+export const acceptTransactionPage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const validId = checkValidId(id);
+    const dataTransaksi = await Transaksi.findOne({
+      where: {id: validId},
+      include: [{
+        model: Murid,
+        attributes: ["nama"]
+
+      },{
+        model: Spp
+      }],
+    })
+
+    const data = dataTransaksi.map((Transaksi) => {
+      return {
+        ...Transaksi.dataValues,
+        id: hashids.encode(Transaksi.id),
+        id_murid: hashids.encode(Transaksi.id_murid),
+        id_spp: hashids.encode(Transaksi.id_spp)
+      };
+    });
+
+    res.render("pages/admin/payment/transaction-request.ejs", {data});
+  } catch (error) {
+    console.log(error.message);
+    res.render("pages/errors/500.ejs");
+  }
+};
   
