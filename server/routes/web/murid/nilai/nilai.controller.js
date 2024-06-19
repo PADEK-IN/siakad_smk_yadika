5b7d66d467c057ec2d976c9d750800ee168d9420
MRoyhanF <froyhan0@gmail.com>
2024-06-19 10:36:05 +0700

    featch list nilai for student role

--- server/routes/web/murid/nilai/nilai.controller.js
+++ server/routes/web/murid/nilai/nilai.controller.js
@@ -1,12 +1,40 @@
-import Murid from "../../../../models/murid.model.js";
+import Murid from '../../../../models/murid.model.js';
+import Penilaian from '../../../../models/penilaian.model.js';
+import Mata_Pelajaran from '../../../../models/mata_pelajaran.model.js';
 import { checkValidId, hashids } from '../../../../helpers/isValidId.js';
 
-export const getNilaiPage = async(req, res) => {
-    try {
-        res.render("pages/murid/nilai/index");
-    } catch (err) {
-        console.log(err.message);
-        res.render('pages/errors/500');
-    }
+export const getNilaiPage = async (req, res) => {
+  try {
+    // const id_murid = 1;
+    // console.log(req.session.user)
+    const id_murid = req.session.user.id_murid;
+    const dataNilai = await Penilaian.findAll({
+      where: {
+        id_murid: id_murid,
+      },
+      include: [
+        {
+          model: Mata_Pelajaran,
+          attributes: ['nama'],
+        },
+      ],
+      raw: true,
+    });
+    // console.log(dataNilai);
 
+    const nilai = dataNilai.map((data) => {
+      return {
+        ...data,
+        id: hashids.encode(data.id),
+        Mata_Pelajaran:
+          data['Mata_Pelajaran.nama'] || 'Tidak ada mata pelajaran',
+      };
+    });
+
+    console.log(nilai);
+    res.render('pages/murid/nilai/index', { nilai });
+  } catch (err) {
+    console.log(err.message);
+    res.render('pages/errors/500');
+  }
 };
