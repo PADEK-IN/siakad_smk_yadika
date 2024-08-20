fc05736bc27101ef363e3d1c7528a22ed03e2d96
fajrCode <fajri42vandi@gmail.com>
2024-08-20 13:33:22 +0700

    fix: signature-name in print nilai murid

--- server/routes/web/murid/nilai/nilai.controller.js
+++ server/routes/web/murid/nilai/nilai.controller.js
@@ -2,11 +2,12 @@ import Murid from '../../../../models/murid.model.js';
 import Penilaian from '../../../../models/penilaian.model.js';
 import Mata_Pelajaran from '../../../../models/mata_pelajaran.model.js';
 import { checkValidId, hashids } from '../../../../helpers/isValidId.js';
+import Kelas from '../../../../models/kelas.model.js';
+import Guru from '../../../../models/guru.model.js';
 
 export const getNilaiPage = async (req, res) => {
   try {
     // const id_murid = 1;
-    // console.log(req.session.user)
     const id_murid = req.session.user.id_murid;
     const dataNilai = await Penilaian.findAll({
       where: {
@@ -22,6 +23,24 @@ export const getNilaiPage = async (req, res) => {
     });
     // console.log(dataNilai);
 
+    const id_wali_kelas = await Murid.findOne({
+      where: {
+        id: id_murid,
+      },
+      include: [
+        {
+          model: Kelas,
+          include: [
+            {
+              model: Guru,
+              attributes: ['nama'],
+            },
+          ],
+        },
+      ],
+      raw: true,
+    });
+
     const nilai = dataNilai.map((data) => {
       return {
         ...data,
@@ -33,7 +52,9 @@ export const getNilaiPage = async (req, res) => {
       };
     });
 
-    res.render('pages/murid/nilai/index', { nilai });
+    const wali_kelas = id_wali_kelas['Kela.Guru.nama'] || 'Tidak ada wali kelas';
+
+    res.render('pages/murid/nilai/index', { nilai, wali_kelas });
   } catch (err) {
     console.log(err.message);
     res.render('pages/errors/500');
